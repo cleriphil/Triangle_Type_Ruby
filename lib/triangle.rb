fe5e4e398ecc6696c21a81455fe2ca2e8cfa6af5
cleri philiastides & Jeff Lalic <student@epicodus.com>
2015-04-27 16:58:39 -0700

    Add and pass Triangle#isosceles? and Triangle#scalene? methods.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,4 +8,20 @@ class Triangle
   define_method(:equilateral?) do
     @side1.==(@side2).&(@side1.==(@side3))
   end
+
+  define_method(:isosceles?) do
+   if @side1.==(@side2).&(@side3.<(@side1.+(@side2)))
+    true
+   elsif @side3.==(@side2).&(@side1.<(@side3.+(@side2)))
+    true
+   elsif @side1.==(@side3).&(@side2.<(@side1.+(@side3)))
+    true
+   else
+    false
+    end
+  end
+
+  define_method(:scalene?) do
+    @side1.!=(@side2).&(@side1.!=(@side3)).&(@side2.!=(@side3))
+  end
 end
--- spec/triable_spec.rb
+++ spec/triable_spec.rb
@@ -3,9 +3,22 @@ require('triangle')
 
 describe(Triangle) do
   describe("#equilateral?") do
-    it('should return true if all sides are equal') do
+    it('returns true if all sides are equal') do
       test_triangle = Triangle.new(5, 5, 5)
       expect(test_triangle.equilateral?()).to(eq(true))
+    end
+  end
+  describe("#isosceles?") do
+    it('returns true if two sides are equal') do
+      test_triangle = Triangle.new(5, 5, 9)
+      expect(test_triangle.isosceles?()).to(eq(true))
+    end
+  end
+
+  describe("#scalene?") do
+    it('returns true if no sides are equal') do
+      test_triangle = Triangle.new(4, 3, 2)
+      expect(test_triangle.scalene?()).to(eq(true))
     end
   end
 end
